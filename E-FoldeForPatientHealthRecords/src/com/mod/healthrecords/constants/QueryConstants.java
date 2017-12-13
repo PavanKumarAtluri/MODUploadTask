853595a0cf25feef768b498ffdcad16349811373
PavanKumarAtluri <pavan.atluri406@gmail.com>
2017-12-13 17:25:59 +0530

    quaries updated

--- E-FoldeForPatientHealthRecords/src/com/mod/healthrecords/constants/QueryConstants.java
+++ E-FoldeForPatientHealthRecords/src/com/mod/healthrecords/constants/QueryConstants.java
@@ -5,7 +5,11 @@ public class QueryConstants {
 	public static final String GET_PATIENT_DETAILS_QUERY="select * from patient_tab where patient_id=?";
 	public static final String GET_DOCTOR_DETAILS_QUERY="select * from doctors_tb where doctor_id=?";
 	public static final String INSERT_PATIENT_HEALTH_RECORD_DETAILS_QUERY="insert into phr_tab values(?,?,?,?,?,?,?,?)";
-	public static final String GET_PHR_DETAILS="select p.PHR_ID,PATIENT_ID,p.DOCTOR_ID,to_char(p.PHR_UPLOADED_DATE,'YYYY-MM-DD HH:mm:SS'), p.PHR_TYPE,p.PHR_UPLOADED_PATH_ORIGINAL,p.PHR_UPLOADED_PATH_PDF,p.PHR_DESCRIPTION,d.DOCTOR_NAME,d.DOCTOR_SPECIALIZATION from phr_tab p, doctors_tab d where p.doctor_id=d.doctor_id and p.patient_id=? and p.phr_type=?";
+	/*public static final String GET_PHR_DETAILS="select p.PHR_ID,PATIENT_ID,p.DOCTOR_ID,to_char(p.PHR_UPLOADED_DATE,'YYYY-MM-DD HH:mm:SS'), p.PHR_TYPE,p.PHR_UPLOADED_PATH_ORIGINAL,p.PHR_UPLOADED_PATH_PDF,p.PHR_DESCRIPTION,d.DOCTOR_NAME,d.DOCTOR_SPECIALIZATION from phr_tab p, doctors_tab d where p.doctor_id=d.doctor_id and p.patient_id=? and p.phr_type=?";
 	public static final String GET_DOCTOR_REPORT="select ph.doctor_id, p.patient_id, p.patient_name, p.patient_age, p.patient_sex, ph.phr_uploaded_date, ph.phr_type, ph.phr_uploaded_path_original, ph.phr_uploaded_path_pdf, ph.phr_description from phr_tab ph inner join patient_tab p on ph.patient_id = p.patient_id where ph.doctor_id=? order by ph.phr_uploaded_date desc";
+	*/
+	
+	public static final String GET_PHR_DETAILS="select p.PHR_ID,PATIENT_ID,p.DOCTOR_ID,to_char(p.PHR_UPLOADED_DATE,'YYYY-MM-DD HH:mm:SS'), p.PHR_TYPE,p.PHR_UPLOADED_PATH_ORIGINAL,p.PHR_UPLOADED_PATH_PDF,p.PHR_DESCRIPTION,d.DOCTOR_NAME,d.DOCTOR_SPECIALIZATION from phr_tab p, doctors_tab d where p.doctor_id=d.doctor_id and p.patient_id=? and p.phr_type=? order by to_char(p.PHR_UPLOADED_DATE,'YYYY-MM-DD HH:mm:SS') desc";
+	public static final String GET_DOCTOR_REPORT="select ph.doctor_id, p.patient_id, p.patient_name, p.patient_age, p.patient_sex, ph.phr_uploaded_date, ph.phr_type, ph.phr_uploaded_path_original, ph.phr_uploaded_path_pdf, ph.phr_description from phr_tab ph inner join patient_tab p on ph.patient_id = p.patient_id where ph.doctor_id=? order by to_char(ph.phr_uploaded_date,'YYYY-MM-DD') desc";
 	public static final String GET_DOCTORS_DETAILS="select DOCTOR_ID,DOCTOR_NAME,DOCTOR_SPECIALIZATION from doctors_tab";
 }
