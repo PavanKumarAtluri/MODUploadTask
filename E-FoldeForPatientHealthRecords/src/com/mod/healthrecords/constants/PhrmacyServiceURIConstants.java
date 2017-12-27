b0ed7f2b223fa96100b365f330345b0ab81a9f1b
PavanKumarAtluri <pavan.atluri406@gmail.com>
2017-12-27 17:49:01 +0530

    commit2 for build_19 after label changes

--- E-FoldeForPatientHealthRecords/src/com/mod/healthrecords/constants/PhrmacyServiceURIConstants.java
+++ E-FoldeForPatientHealthRecords/src/com/mod/healthrecords/constants/PhrmacyServiceURIConstants.java
@@ -1,5 +1,5 @@
 package com.mod.healthrecords.constants;
 
 public class PhrmacyServiceURIConstants {
-	public static final String SEND_DOCTOR_PRESCRIPTION="http://localhost:8090/ParmacyService/phrmacyProvider/submitDoctorPrescription";
+	public static final String SEND_DOCTOR_PRESCRIPTION="http://localhost:8091/ParmacyService/phrmacyProvider/submitDoctorPrescription";
 }
