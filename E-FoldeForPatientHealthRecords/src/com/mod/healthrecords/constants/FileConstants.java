d5873d42cab4156ecf6fc70f7050362f8bd25b56
PavanKumarAtluri <pavan.atluri406@gmail.com>
2018-01-23 12:38:26 +0530

    commit for local use

--- E-FoldeForPatientHealthRecords/src/com/mod/healthrecords/constants/FileConstants.java
+++ E-FoldeForPatientHealthRecords/src/com/mod/healthrecords/constants/FileConstants.java
@@ -1,6 +1,6 @@
 package com.mod.healthrecords.constants;
 
 public class FileConstants {
-		public static final String PDF_FILE_DEST_LOC = "d:/springfileuploads/pdf";
-		public static final String ORIGINAL_FILE__DEST_LOC = "d:/springfileuploads/original";
+		public static final String PDF_FILE_DEST_LOC = "e:/springfileuploads/pdf";
+		public static final String ORIGINAL_FILE__DEST_LOC = "e:/springfileuploads/original";
 }
