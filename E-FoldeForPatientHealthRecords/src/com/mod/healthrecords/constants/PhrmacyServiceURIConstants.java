ee4fcc76b48701890541ea2223480092adbad0de
PavanKumarAtluri <pavan.atluri406@gmail.com>
2018-01-31 18:52:48 +0530

    commit after adding ui changes in search components for 19s

--- E-FoldeForPatientHealthRecords/src/com/mod/healthrecords/constants/PhrmacyServiceURIConstants.java
+++ E-FoldeForPatientHealthRecords/src/com/mod/healthrecords/constants/PhrmacyServiceURIConstants.java
@@ -1,12 +1,12 @@
 package com.mod.healthrecords.constants;
 
 public class PhrmacyServiceURIConstants {
-	public static final String SEND_DOCTOR_PRESCRIPTION_URL="http://localhost:8085/ParmacyService/phrmacyProvider/submitDoctorPrescription";
-	public static final String GET_ORDERS_OF_PARTICULAR_PATIENT_URL="http://localhost:8085/ParmacyService/phrmacyProvider/getOrderOfParticularPatient?pharmacyId={pharmacyId}&patientId={patientId}";
-	public static final String GET_ORDER_BY_ORDERID_URL="http://localhost:8085/ParmacyService/phrmacyProvider/getOrderByOrderId/{pharmacyId}/{orderId}";
-	public static final String GET_ALL_ORDERS_OF_PHARMACY_URL="http://localhost:8085/ParmacyService/phrmacyProvider/getAllOrdersByPharmacyId/{pharmacyId}";
-	public static final String CHANGE_DELIVERY_STATUS="http://localhost:8085/ParmacyService/phrmacyProvider/changeDeliveryStatusByDeliveryId/{deliveryId}";
-	public static final String CHANGE_PAYMENT_STATUS="http://localhost:8085/ParmacyService/phrmacyProvider/changePaymentStatusByDeliveryId/{deliveryId}";
-	public static final String GET_ORDERSTATUS_BYPHRID="http://localhost:8085/ParmacyService/phrmacyProvider/getOrderStatusByPHRID?phrId={phrId}";
+	public static final String SEND_DOCTOR_PRESCRIPTION_URL="http://localhost:8091/ParmacyService/phrmacyProvider/submitDoctorPrescription";
+	public static final String GET_ORDERS_OF_PARTICULAR_PATIENT_URL="http://localhost:8091/ParmacyService/phrmacyProvider/getOrderOfParticularPatient?pharmacyId={pharmacyId}&patientId={patientId}";
+	public static final String GET_ORDER_BY_ORDERID_URL="http://localhost:8091/ParmacyService/phrmacyProvider/getOrderByOrderId/{pharmacyId}/{orderId}";
+	public static final String GET_ALL_ORDERS_OF_PHARMACY_URL="http://localhost:8091/ParmacyService/phrmacyProvider/getAllOrdersByPharmacyId/{pharmacyId}";
+	public static final String CHANGE_DELIVERY_STATUS="http://localhost:8091/ParmacyService/phrmacyProvider/changeDeliveryStatusByDeliveryId/{deliveryId}";
+	public static final String CHANGE_PAYMENT_STATUS="http://localhost:8091/ParmacyService/phrmacyProvider/changePaymentStatusByDeliveryId/{deliveryId}";
+	public static final String GET_ORDERSTATUS_BYPHRID="http://localhost:8091/ParmacyService/phrmacyProvider/getOrderStatusByPHRID?phrId={phrId}";
 	public static final String SEND_SMS_PRESCRIPTION_URL="http://2factor.in/API/V1/e9c55599-05c3-11e8-a328-0200cd936042/ADDON_SERVICES/SEND/TSMS";
 }
